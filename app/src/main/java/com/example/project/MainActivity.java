b098511991a278a5603354e03fad3f8f813f6ff3
Chernov2312 <m.chernov_2008@mail.ru>
2023-05-20 18:17:26 +0700

    Update project

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -41,8 +41,7 @@ public class MainActivity extends AppCompatActivity {
             this.finishAffinity();
         });
         binding.load.setOnClickListener(v -> {
-            load2();
-            Snackbar.make(binding.load, getResources().getString(R.string.loadsave), Snackbar.LENGTH_SHORT).show();
+            game();
             d = 1;
             recreate();
         });
@@ -132,43 +131,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void load2() {
-        Character_Settings.setGolds(Settings.sharedPreferences.getInt(Settings.GOLDS_PREFERENCE, Character_Settings.getGolds()));
-        Toast.makeText(this, ""+Character_Settings.getGolds(),
-                Toast.LENGTH_LONG).show();
-        Character_Settings.setLevel(Settings.sharedPreferences.getInt(Settings.LEVEL_PREFERENCE, Character_Settings.getLevel()));
-        Character_Settings.setXit_points(Settings.sharedPreferences.getInt(Settings.XIT_POINTS_PREFERENCE, Character_Settings.getXit_points()));
-        Character_Settings.setMaxXit_points(Settings.sharedPreferences.getInt(Settings.MAXXITPOINTS_PREFERENCE, Character_Settings.getMaxXit_points()));
-        Character_Settings.setFood(Settings.sharedPreferences.getInt(Settings.FOOD_PREFERENCE, Character_Settings.getFood()));
-        Character_Settings.setWater(Settings.sharedPreferences.getInt(Settings.WATER_PREFERENCE, Character_Settings.getWater()));
-        Character_Settings.setMaxmoney(Settings.sharedPreferences.getInt(Settings.MAXMONEY_PREFERENCE, Character_Settings.getMaxmoney()));
-        Character_Settings.setKilledmobs(Settings.sharedPreferences.getInt(Settings.kILLEDMOBS_PREFERENCE, Character_Settings.getKilledmobs()));
-        Character_Settings.setTotal_money(Settings.sharedPreferences.getInt(Settings.TOTAL_MONEY_PREFERENCE, Character_Settings.getTotal_money()));
-        Character_Settings.setDamage(Settings.sharedPreferences.getInt(Settings.DAMAGE_PREFERENCE, Character_Settings.getDamage()));
-        Character_Settings.setBlockdamage(Settings.sharedPreferences.getInt(Settings.BLOCKDAMAGE_PREFERENCE, Character_Settings.getBlockdamage()));
-        Character_Settings.setLvlmech(Settings.sharedPreferences.getInt(Settings.LVLMECH_PREFERENCE, Character_Settings.getLvlmech()));
-        Character_Settings.setLvlshield(Settings.sharedPreferences.getInt(Settings.LVLSHIELD_PREFERENCE, Character_Settings.getLvlshield()));
-        Character_Settings.setLvlmag(Settings.sharedPreferences.getInt(Settings.LVLMAG_PREFERENCE, Character_Settings.getLvlmag()));
-        Character_Settings.setLvlarmor(Settings.sharedPreferences.getInt(Settings.LVLARMOR_PREFERENCE, Character_Settings.getLvlarmor()));
-        Settings.Foodff[0] = Settings.sharedPreferences.getInt(Settings.FOODF1_PREFERENCE, Character_Settings.getFoodf()[0]);
-        Settings.Foodff[1] = Settings.sharedPreferences.getInt(Settings.FOODF2_PREFERENCE, Character_Settings.getFoodf()[1]);
-        Settings.Foodff[2] = Settings.sharedPreferences.getInt(Settings.FOODF3_PREFERENCE, Character_Settings.getFoodf()[2]);
-        Settings.Waterff[0] = Settings.sharedPreferences.getInt(Settings.WATER1_PREFERENCE, Character_Settings.getWaterr()[0]);
-        Settings.Waterff[1] = Settings.sharedPreferences.getInt(Settings.WATER2_PREFERENCE, Character_Settings.getWaterr()[1]);
-        Settings.Waterff[2] = Settings.sharedPreferences.getInt(Settings.WATER3_PREFERENCE, Character_Settings.getWaterr()[2]);
-        Settings.Hillff[0] = Settings.sharedPreferences.getInt(Settings.HILL1_PREFERENCE, Character_Settings.getHill()[0]);
-        Settings.Hillff[1] = Settings.sharedPreferences.getInt(Settings.HILL2_PREFERENCE, Character_Settings.getHill()[1]);
-        Settings.Hillff[2] = Settings.sharedPreferences.getInt(Settings.HILL3_PREFERENCE, Character_Settings.getHill()[2]);
-        setMenu(Settings.sharedPreferences.getInt(Settings.MENU_PREFERENCE, getMenu()));
-        Character_Settings.setS(Settings.sharedPreferences.getBoolean(Settings.S_PREFERENCE, Character_Settings.getS()));
-        Home.setBoxcheked(Settings.sharedPreferences.getInt(Settings.BOXCHEKED, Home.getBoxcheked()));
-        BossFight.setBossXitpoints(Settings.sharedPreferences.getInt(Settings.BOSS_XIT_POINTS, BossFight.getBossXitpoints()));
-        Easyandmedium.setMob_xit_points(Settings.sharedPreferences.getInt(Settings.MOB_XIT_POINTS, Easyandmedium.getMob_xit_points()));
-        Easyandmedium.setMob(Settings.sharedPreferences.getInt(Settings.MOB, Easyandmedium.getMob()));
-        Easyandmedium.setMob_damage(Settings.sharedPreferences.getInt(Settings.MOB_DAMAGE, Easyandmedium.getMob_damage()));
+        Character_Settings.setGolds(Settings.getSharedPreferences().getInt(Settings.GOLDS_PREFERENCE, Character_Settings.getGolds()));
+        Character_Settings.setLevel(Settings.getSharedPreferences().getInt(Settings.LEVEL_PREFERENCE, Character_Settings.getLevel()));
+        Character_Settings.setXit_points(Settings.getSharedPreferences().getInt(Settings.XIT_POINTS_PREFERENCE, Character_Settings.getXit_points()));
+        Character_Settings.setMaxXit_points(Settings.getSharedPreferences().getInt(Settings.MAXXITPOINTS_PREFERENCE, Character_Settings.getMaxXit_points()));
+        Character_Settings.setFood(Settings.getSharedPreferences().getInt(Settings.FOOD_PREFERENCE, Character_Settings.getFood()));
+        Character_Settings.setWater(Settings.getSharedPreferences().getInt(Settings.WATER_PREFERENCE, Character_Settings.getWater()));
+        Character_Settings.setMaxmoney(Settings.getSharedPreferences().getInt(Settings.MAXMONEY_PREFERENCE, Character_Settings.getMaxmoney()));
+        Character_Settings.setKilledmobs(Settings.getSharedPreferences().getInt(Settings.kILLEDMOBS_PREFERENCE, Character_Settings.getKilledmobs()));
+        Character_Settings.setTotal_money(Settings.getSharedPreferences().getInt(Settings.TOTAL_MONEY_PREFERENCE, Character_Settings.getTotal_money()));
+        Character_Settings.setDamage(Settings.getSharedPreferences().getInt(Settings.DAMAGE_PREFERENCE, Character_Settings.getDamage()));
+        Character_Settings.setBlockdamage(Settings.getSharedPreferences().getInt(Settings.BLOCKDAMAGE_PREFERENCE, Character_Settings.getBlockdamage()));
+        Character_Settings.setLvlmech(Settings.getSharedPreferences().getInt(Settings.LVLMECH_PREFERENCE, Character_Settings.getLvlmech()));
+        Character_Settings.setLvlshield(Settings.getSharedPreferences().getInt(Settings.LVLSHIELD_PREFERENCE, Character_Settings.getLvlshield()));
+        Character_Settings.setLvlmag(Settings.getSharedPreferences().getInt(Settings.LVLMAG_PREFERENCE, Character_Settings.getLvlmag()));
+        Character_Settings.setLvlarmor(Settings.getSharedPreferences().getInt(Settings.LVLARMOR_PREFERENCE, Character_Settings.getLvlarmor()));
+        Settings.Foodff[0] = Settings.getSharedPreferences().getInt(Settings.FOODF1_PREFERENCE, Character_Settings.getFoodf()[0]);
+        Settings.Foodff[1] = Settings.getSharedPreferences().getInt(Settings.FOODF2_PREFERENCE, Character_Settings.getFoodf()[1]);
+        Settings.Foodff[2] = Settings.getSharedPreferences().getInt(Settings.FOODF3_PREFERENCE, Character_Settings.getFoodf()[2]);
+        Settings.Waterff[0] = Settings.getSharedPreferences().getInt(Settings.WATER1_PREFERENCE, Character_Settings.getWaterr()[0]);
+        Settings.Waterff[1] = Settings.getSharedPreferences().getInt(Settings.WATER2_PREFERENCE, Character_Settings.getWaterr()[1]);
+        Settings.Waterff[2] = Settings.getSharedPreferences().getInt(Settings.WATER3_PREFERENCE, Character_Settings.getWaterr()[2]);
+        Settings.Hillff[0] = Settings.getSharedPreferences().getInt(Settings.HILL1_PREFERENCE, Character_Settings.getHill()[0]);
+        Settings.Hillff[1] = Settings.getSharedPreferences().getInt(Settings.HILL2_PREFERENCE, Character_Settings.getHill()[1]);
+        Settings.Hillff[2] = Settings.getSharedPreferences().getInt(Settings.HILL3_PREFERENCE, Character_Settings.getHill()[2]);
+        setMenu(Settings.getSharedPreferences().getInt(Settings.MENU_PREFERENCE, getMenu()));
+        Character_Settings.setS(Settings.getSharedPreferences().getBoolean(Settings.S_PREFERENCE, Character_Settings.getS()));
+        Home.setBoxcheked(Settings.getSharedPreferences().getInt(Settings.BOXCHEKED, Home.getBoxcheked()));
+        BossFight.setBossXitpoints(Settings.getSharedPreferences().getInt(Settings.BOSS_XIT_POINTS, BossFight.getBossXitpoints()));
+        Easyandmedium.setMob_xit_points(Settings.getSharedPreferences().getInt(Settings.MOB_XIT_POINTS, Easyandmedium.getMob_xit_points()));
+        Easyandmedium.setMob(Settings.getSharedPreferences().getInt(Settings.MOB, Easyandmedium.getMob()));
+        Easyandmedium.setMob_damage(Settings.getSharedPreferences().getInt(Settings.MOB_DAMAGE, Easyandmedium.getMob_damage()));
         Character_Settings.setFoodd(Settings.Foodff);
-        Settings.music = Settings.sharedPreferences.getBoolean(Settings.MUSIC, Settings.music);
+        Settings.music = Settings.getSharedPreferences().getBoolean(Settings.MUSIC, Settings.music);
         Character_Settings.setWaterr(Settings.Waterff);
         Character_Settings.setHill(Settings.Hillff);
-        Character_Settings.setInventoryy(new ArrayList<>(Settings.sharedPreferences.getStringSet(Settings.INVENTORYY_PREFERENCE, Settings.set)));
+        Character_Settings.setInventoryy(new ArrayList<>(Settings.getSharedPreferences().getStringSet(Settings.INVENTORYY_PREFERENCE, Settings.set)));
     }
 }
